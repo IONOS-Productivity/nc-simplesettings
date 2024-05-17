5f89e039577dcd1a0eab4601f5c5a9dd51838d22
Thomas Lehmann <t.lehmann@strato-rz.de>
2024-05-17 11:38:36 +0200

    main: import ./store/authtoken to fix resolve error
    This fixes
    
       ERROR in ./src/App.vue?vue&type=script&lang=ts (./node_modules/babel-loader/lib/index.js!./node_modules/ts-loader/index.js!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/App.vue?vue&type=script&lang=ts) 29:0-31
       Module not found: Error: Can't resolve '../logger' in '/app/src'
    
    This error occured after re-adding  lang="ts"  to App.vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// Import without use to prevent odd Webpack resolve error
+//    ERROR in ./src/App.vue?vue&type=script&lang=ts (./node_modules/babel-loader/lib/index.js!./node_modules/ts-loader/index.js!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/App.vue?vue&type=script&lang=ts) 29:0-31
+//    Module not found: Error: Can't resolve '../logger' in '/app/src'
+// eslint-disable-next-line
+import { useAuthTokenStore } from './store/authtoken'
 
 declare function n(a: string): string;
 declare function t(a: string): string;
