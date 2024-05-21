30d156025c3e23ebe94968321c5ec5db1edf956b
Thomas Lehmann <t.lehmann@strato-rz.de>
2024-05-21 08:47:46 +0200

    Adapt generate Vue config to use nextcloud-vite-config
    Using the config generator function from [1].
    
    * Followed the example of [1]
    * Added @nextcloud/vite-config on top of the generated configs
      (the Vue+Vite config was generated using "npm create vue"
       in a previous commit)
    * Due to lack of documentation, I looked at the source [2]
    * vitest.config.ts adapted to use the vite.config.ts
    
    [1]: https://github.com/nextcloud-libraries/nextcloud-vite-config
    [2]: https://github.com/nextcloud-libraries/nextcloud-vite-config/blob/v2.0.0/lib/appConfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
 import path from 'node:path'
+import { createAppConfig } from '@nextcloud/vite-config'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        'main': path.resolve(__dirname, 'src/main.ts'),
-      },
-    },
-  },
+// See
+// - https://github.com/nextcloud-libraries/nextcloud-vite-config/tree/v2.0.0
+// - https://github.com/nextcloud-libraries/nextcloud-vite-config/blob/v2.0.0/lib/appConfig.ts
+export default createAppConfig({
+    // entry points: {name: script}
+    main: path.resolve(__dirname, 'src/main.ts'),
+}, {
+    inlineCSS: true,
+    config: () => ({
+      build: {
+        // The dist filename /js/main.js is decided by
+        // - folder: nextcloud:lib/public/Util::addScript() (assumed to be in /js/)
+        // - filename: app:/template/mindex.php (explicitly passed to addScript())
+        outDir: 'js',
+        rollupOptions: {
+          output: {
+             entryFileNames: () => 'main.js',
+          }
+        }
+      }
+    })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import vue from '@vitejs/plugin-vue'
 
 export default mergeConfig(
-  viteConfig,
+  viteConfig({ mode: 'development', command: 'build' }),
   defineConfig({
+    plugins: [
+      // This was necessary to fix
+      // "Error: Failed to parse source for import analysis because the content contains invalid JS syntax. Install @vitejs/plugin-vue to handle .vue files."
+      // I assumed this was already defined in viteConfig and retained in merge
+      vue(),
+    ],
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
