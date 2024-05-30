fbcc2c3022291c36e234dc4f7d16ca41d6734866
Thomas Lehmann <t.lehmann@strato-rz.de>
2024-05-30 10:51:14 +0200

    webpack: specify public sourcemap location, make .map loadable
    * configure Webpack to tell the public path of the .map
    * add controller to load the .map

---
+++ lib/Controller/MainJsSourcemapController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OCA\SimpleSettings\Controller;
+
+use OCA\SimpleSettings\Config;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\DataDownloadResponse;
+use OCP\AppFramework\Http\TextPlainResponse;
+use OCP\IConfig;
+use OCP\IRequest;
+
+class MainJsSourcemapController extends Controller {
+
+
+	/**
+	 * constructor of the controller
+	 *
+	 * @param string $appName
+	 * @param IRequest $request
+	 */
+	public function __construct($appName,
+								IRequest $request) {
+		parent::__construct($appName, $request);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 * @PublicPage
+	 *
+	 * @return DataDownloadResponse
+	 */
+	public function sourceMap(): DataDownloadResponse {
+		$script = file_get_contents(__DIR__ . '/../../js/main.js.map');
+		return new DataDownloadResponse($script, 'main.js.map', 'text/javascript');
+	}
+}
