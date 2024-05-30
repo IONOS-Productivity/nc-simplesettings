67014dc13042a07b1f57e6e21e8005580c308ad7
Thomas Lehmann <t.lehmann@strato-rz.de>
2024-05-30 13:54:13 +0200

    main.ts: register pinia
    Fixes
    
       TypeError: Cannot read properties of undefined (reading '_s')
           at i (pinia.mjs:1714:20)
           at setup (AuthTokenList.vue:11:32)
           at mn (vue.runtime.esm.js:3033:30)
           at vue.runtime.esm.js:2457:27
           at Fa (vue.runtime.esm.js:5385:5)
           at e._init (vue.runtime.esm.js:5714:9)
           at new s (vue.runtime.esm.js:5849:18)
           at vue.runtime.esm.js:4564:12
           at init (vue.runtime.esm.js:4426:52)
           at vue.runtime.esm.js:6591:17
    
    When stepping (stumbling) through the code one can see that at some
    point Pinia is not defined.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import { PiniaVuePlugin, createPinia } from 'pinia'
 import App from './App.vue'
 
 declare function n(a: string): string;
 declare function t(a: string): string;
 
+const pinia = createPinia()
+
+Vue.use(PiniaVuePlugin)
+
 Vue.mixin({ methods: { t, n } })
 
 const View = Vue.extend(App)
-new View().$mount('#simplesettings')
+new View({ pinia }).$mount('#simplesettings')
