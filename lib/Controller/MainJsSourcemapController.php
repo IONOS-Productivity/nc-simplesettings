cb6cd148c93a2fff824085940999e67a145f4e04
Thomas Lehmann <t.lehmann@strato-rz.de>
2024-06-10 13:50:34 +0200

    [WIP] MainJsSourcemapController.php: remove unused imports

--- lib/Controller/MainJsSourcemapController.php
+++ lib/Controller/MainJsSourcemapController.php
@@ -1,12 +1,27 @@
 <?php
+/**
+ * SPDX-FileLicenseText: 2024 Thomas Lehmann <t.lehmann@strato.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 namespace OCA\SimpleSettings\Controller;
 
-use OCA\SimpleSettings\Config;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\DataDownloadResponse;
-use OCP\AppFramework\Http\TextPlainResponse;
-use OCP\IConfig;
 use OCP\IRequest;
 
 class MainJsSourcemapController extends Controller {
