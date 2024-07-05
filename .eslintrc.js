f092e8c547cce4fcee4938d15a2d6515fa93efb1
Thomas Lehmann <t.lehmann@strato.de>
2024-07-05 15:33:51 +0200

    .eslintrc.js: disable rule vue/order-in-components
    It conflicts with a necessary workaround for a tsc bug fixed in the
    previous commit.
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,9 @@ module.exports = {
 	rules: {
 		'jsdoc/require-jsdoc': 'off',
 		'vue/first-attribute-linebreak': 'off',
+		// Workaround due to a bug in tsc, which fails with "property foo
+		// does not exist", which is clearly present
+		// See https://github.com/vuejs/vue/issues/12628#issuecomment-1283730746
+		'vue/order-in-components': 'off',
 	},
 }
