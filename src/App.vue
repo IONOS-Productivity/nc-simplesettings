42a86c955792c67ed48876b1d47015113edd9d09
Thomas Lehmann <t.lehmann@strato.de>
2024-07-05 15:33:51 +0200

    components: add WebDavUrl component
    Copy from Nextcloud's /apps/settings with slight modifications
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- src/App.vue
+++ src/App.vue
@@ -17,25 +17,35 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <template>
-	<AuthTokenSection />
+	<content>
+		<AuthTokenSection />
+		<hr>
+		<WebDavUrl />
+	</content>
 </template>
 
 <script lang="ts">
 import AuthTokenSection from './components/security/AuthTokenSection.vue'
+import WebDavUrl from './components/files/WebDavUrl.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
 	name: 'App',
 	components: {
 		AuthTokenSection,
+		WebDavUrl,
 	},
 })
 </script>
 
 <style scoped lang="scss">
-#simplesettings {
-	display: flex;
-	justify-content: center;
-	margin: 16px;
+content {
+	width: 100%;
+	overflow-y: scroll;
+	background-color: var(--color-main-background);
+}
+
+hr {
+	margin: 0 30px;
 }
 </style>
