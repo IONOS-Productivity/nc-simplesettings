87a4afb846e3d11c3845714fc9eb7d38a28da5f6
Thomas Lehmann <t.lehmann@strato.de>
2024-07-05 15:33:51 +0200

    PageController: provide initial state (with user-id hardcoded)
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,9 +1,30 @@
 <?php
+/**
+ * SPDX-FileLicenseText: 2024 Thomas Lehmann <t.lehmann@strato.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 declare(strict_types=1);
 
 namespace OCA\SimpleSettings\Controller;
 
+use OC\Authentication\Token\INamedToken;
+use OC\Authentication\Token\IProvider as IAuthTokenProvider;
+use OC\Authentication\Token\IToken;
 use OCA\SimpleSettings\AppInfo\Application;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\Attribute\FrontpageRoute;
@@ -11,19 +32,87 @@ use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
+use OCP\Authentication\Exceptions\InvalidTokenException;
+use OCP\ISession;
+use OCP\IUserSession;
+use OCP\Session\Exceptions\SessionNotAvailableException;
 
 /**
  * @psalm-suppress UnusedClass
  */
 class PageController extends Controller {
+	/** @var IInitialState */
+	private $initialState;
+
+	/** @var IUserSession */
+	private $userSession;
+
+	/** @var ISession */
+	private $session;
+
+	/** @var string|null */
+	private $uid;
+
+	public function __construct(
+		IAuthTokenProvider $tokenProvider,
+		ISession $session,
+		IInitialState $initialState,
+		IUserSession $userSession,
+		?string $UserId
+	) {
+		$this->tokenProvider = $tokenProvider;
+		$this->session = $session;
+		$this->initialState = $initialState;
+		$this->userSession = $userSession;
+		$this->uid = $UserId;
+	}
+
 	#[NoCSRFRequired]
 	#[NoAdminRequired]
 	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
 	#[FrontpageRoute(verb: 'GET', url: '/')]
 	public function index(): TemplateResponse {
+		$this->initialState->provideInitialState(
+			'app_tokens',
+			$this->getAppTokens()
+		);
+
+		$this->initialState->provideInitialState(
+			'can_create_app_token',
+			$this->userSession->getImpersonatingUserID() === null
+		);
+
 		return new TemplateResponse(
 			Application::APP_ID,
 			'index',
 		);
+	}
+
+	private function getAppTokens(): array {
+		$tokens = $this->tokenProvider->getTokenByUser($this->uid);
+
+		try {
+			$sessionId = $this->session->getId();
+		} catch (SessionNotAvailableException $ex) {
+			return [];
+		}
+		try {
+			$sessionToken = $this->tokenProvider->getToken($sessionId);
+		} catch (InvalidTokenException $ex) {
+			return [];
+		}
+
+		return array_map(function (IToken $token) use ($sessionToken) {
+			$data = $token->jsonSerialize();
+			$data['canDelete'] = true;
+			$data['canRename'] = $token instanceof INamedToken && $data['type'] !== IToken::WIPE_TOKEN;
+			if ($sessionToken->getId() === $token->getId()) {
+				$data['canDelete'] = false;
+				$data['canRename'] = false;
+				$data['current'] = true;
+			}
+			return $data;
+		}, $tokens);
 	}
 }
