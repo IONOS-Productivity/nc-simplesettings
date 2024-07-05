e5a3ce55ac0ac2aba554552993c4c68a8959d15b
Thomas Lehmann <t.lehmann@strato.de>
2024-07-05 15:33:51 +0200

    Change to typescript
    * Add tsconfig.json (from Nextcloud, with some modifications)
      * "include" patterns adapted
      * "jest" removed from types for now (no tests yet)
      * types reduced to ["vue"]
      * vueCompilerOptions omitted
        * removing it does not change the outcome
        * guides on Webpack + Vue + Typescript don't show it
        * probably only necessary for the VScode extension "Volar" [1]
      * ts-node omitted
        * we're not compiling for node environments
      * set allowImportingTsExtensions and noEmit false
    
        To prevent
    
           ERROR in ./src/main.ts
           Module build failed (from ./node_modules/ts-loader/index.js):
           Error: TypeScript emitted no output for /app/src/main.ts.
               at makeSourceMapAndFinish (/app/node_modules/ts-loader/dist/index.js:55:18)
               at successLoader (/app/node_modules/ts-loader/dist/index.js:42:5)
               at Object.loader (/app/node_modules/ts-loader/dist/index.js:23:5)
    
      A helpful resource was [2]
    
    * webpack.js: added appendTsSuffixTo option
    
      To tell Webpack to pass .vue thorugh the Typescript compiler too
    
    * Add shims-vue.d.ts with re-export of Vue types
    
    * Installed @vue/tsconfig because it's referenced by tsconfig.json
    
    * Adapted filename pattern for entrypoint to .ts
    
    * Added type declaration for "n" and "t" because in the Javascript they
      were referenced as globals (this should later be changed to some
      proper import of a typedef)
    
    * main.ts: l10n was added to explicitly pass typed references, not rely
      on globals
    
    [1]: https://v2.vuejs.org/v2/guide/migration-vue-2-7#Volar-Compatibility
    [2]: https://github.com/nextcloud/cookbook/pull/2059/files?diff=unified&w=1#diff-b55cdbef4907b7045f32cc5360d48d262cca5f94062e353089f189f4460039e0
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
+import { translate as t, translatePlural as n } from '@nextcloud/l10n'
 import App from './App.vue'
+
 Vue.mixin({ methods: { t, n } })
 
 const View = Vue.extend(App)
--- webpack.js
+++ webpack.js
@@ -4,7 +4,7 @@ const StyleLintPlugin = require('stylelint-webpack-plugin')
 const path = require('path')
 
 webpackConfig.entry = {
-	main: { import: path.join(__dirname, 'src', 'main.js'), filename: 'main.js' },
+	main: { import: path.join(__dirname, 'src', 'main.ts'), filename: 'main.js' },
 }
 
 webpackConfig.plugins.push(
@@ -18,6 +18,30 @@ webpackConfig.plugins.push(
 		files: 'src/**/*.{css,scss,vue}',
 	}),
 )
+
+// Modify Webpack config to patch .tsx? rule
+// The ts-loader option appendTsSuffixTo: [/\.vue$/] is required, otherwise
+// it would emit no transpiler result for <script lang="ts"> blocks in .vue
+// files
+webpackConfig.module.rules = webpackConfig.module.rules.map((module) => {
+	if (!Array.isArray(module.use) || !module.use.includes('ts-loader')) {
+		return module
+	}
+
+	return {
+		test: /\.tsx?$/,
+		use: [
+			'babel-loader',
+			{
+				loader: 'ts-loader',
+				options: {
+					appendTsSuffixTo: [/\.vue$/]
+				}
+			}
+		],
+		exclude: /node_modules/
+	}
+})
 
 webpackConfig.module.rules.push({
 	test: /\.svg$/i,
