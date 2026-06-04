e21d0ad79751e0499f5a32e256c1924541fb7a24
Tatjana Kaschperko Lindt <kaschperko-lindt@strato.de>
2026-06-04 14:25:23 +0200

    fix(Software.vue): consolidate gap with same units (em)
    Signed-off-by: Tatjana Kaschperko Lindt <kaschperko-lindt@strato.de>

--- src/components/help/Software.vue
+++ src/components/help/Software.vue
@@ -146,7 +146,7 @@ export default defineComponent({
 
 #software {
 	--software-content-width: 40em;
-	--software-gap: 10px;
+	--software-gap: 1em;
 	--software-qr-code-intrinsic-padding: 5px; /* padding within the image around the code (measured) */
 }
 
@@ -180,7 +180,7 @@ export default defineComponent({
 	display: flex;
 	flex-wrap: wrap;
 	flex: 0 0 auto;
-	gap: 1em;
+	gap: var(--software-gap);
 }
 
 .symbol {
